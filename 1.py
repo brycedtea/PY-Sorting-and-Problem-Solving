79c523e120fd05c4602ce442a11218b08199b8a8
Admin <brycealvir@gmail.com>
2022-10-02 09:41:25 -0700

    sorting algos

--- 1.py
+++ 1.py
@@ -1,1 +1,18 @@
-# Write your solution for algorithm 1 below
+# sort() will sort the list in-place, mutating its indexes and returning None , whereas sorted() will return a new sorted list leaving the original list unchanged
+
+# 1. Write an algorithm that takes in an unsorted numerical list as an argument and sorts the list (use the sort() method).
+lst = [6, 3, 1, 2]
+lst.sort()
+ 
+print(lst)
+
+# 2. Write an algorithm that takes in an unsorted numerical list as an argument and creates a new sorted list (use the sorted() function).
+
+unsorted_list = [6, 3, 1, 2]
+sorted_list = sorted(unsorted_list)
+print(sorted_list)
+#3. Write an algorithm that takes in an unsorted numerical list as an argument and creates a new sorted list in descending order (use the sorted() function).
+
+unsorted_list = [6, 3, 1, 2]
+sorted_list = sorted(unsorted_list, reverse=True)
+print(sorted_list)
--- Bonus/4.py
+++ Bonus/4.py
@@ -1,1 +1,12 @@
-# Write your solution for algorithm 4 below
+# 4. Write an algorithm that takes in a string and sorts the words in the string in alphabetical order. The comparison should be case-insensitive.
+
+"""
+Sample input: 'I love software engineering'
+Sample output: ['engineering', 'I', 'love', 'software']
+Note that a key parameter will need to be used here in order for the sorting to be case-insensitive (sort in alphabetical order regardless of whether string is uppercase or lowercase).
+"""
+
+test_string = "I love software engineering"
+
+sorted_strings = sorted(test_string.split(), key = str.lower)
+print(sorted_strings)
